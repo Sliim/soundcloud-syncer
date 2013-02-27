2ed8397e820f4a5d22c8008b98fa0cba6b61c8a6
Sliim <sliim@mailoo.org>
2013-02-27 01:12:21 +0100

    Use argparse for sync script (require parameters: user, client_id, output_directory).

--- sync.py
+++ sync.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3.2
+import argparse
+import os.path
 
 from ssyncer.sclient import sclient
 from ssyncer.suser import suser
 
-user      = "YOU"
-client_id = "YOUR_CLIENT_ID"
-local_dir = "OUTPUT_DIRECTORY"
+parser = argparse.ArgumentParser(description="Soundcloud Syncer")
+parser.add_argument("-u", "--user", help="Soundcloud user to sync", type=str, required=True)
+parser.add_argument("-c", "--client-id", help="Your client id", type=str, required=True)
+parser.add_argument("-o", "--output-dir", help="Output directory", type=str, required=True)
+args = parser.parse_args()
 
-sclient = sclient(client_id)
-suser = suser(user, client=sclient)
+if not os.path.exists(args.output_dir):
+    print("Error: output directory `%s` doesn's exists." % args.output_dir)
+    exit(1)
+
+sclient = sclient(args.client_id)
+suser = suser(args.user, client=sclient)
 
 likes = suser.get_likes()
 if not likes:
     print("ERROR: Can't get user's likes!'")
-    exit(1)
+    exit(2)
 
 for strack in likes:
-    strack.download(local_dir)
+    print("Start downloading %s.." % strack.get("title"))
+    strack.download(args.output_dir)
