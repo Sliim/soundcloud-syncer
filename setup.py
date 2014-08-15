2fb57366b86bec90b55894a63af29590a62ece0c
Sliim <sliim@mailoo.org>
2014-08-15 04:09:03 +0200

    Bump version 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="soundcloud-syncer",
-    version="0.2.2",
+    version="0.2.3",
     description="Synchronize user's favorites tracks from soundcloud",
     author="Sliim",
     author_email="sliim@mailoo.org",
